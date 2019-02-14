68fb146ede84abe9dc1c2c20d0775b693dd4df79
Guilherme Rodrigues <guilhermerodriguestb@gmail.com>
2019-02-14 16:49:04 -0200

    Update Hidrate.php

--- Hidrate.php
+++ Hidrate.php
@@ -5,7 +5,7 @@ trait Hidrate {
         foreach ($valores as $atributo => $value) {
             $metodoSet = 'set' . ucfirst($atributo);
             if (method_exists($this, $metodoSet)) {
-                    $this->{$metodoSet}($value);
+                $this->{$metodoSet}($value);
             }
         }
     }
