bfd85082edde82df3d99d3fa24b46bce6a91af77
Guilherme Rodrigues <guilhermerodriguestb@gmail.com>
2019-02-15 16:07:09 -0200

    Update Hidrate.php

--- Hidrate.php
+++ Hidrate.php
@@ -1,11 +1,11 @@
 <?php
 
-trait Hidrate {
-    public function populate(array $valores) {
-        foreach ($valores as $atributo => $value) {
-            $metodoSet = 'set' . ucfirst($atributo);
-            if (method_exists($this, $metodoSet)) {
-                $this->{$metodoSet}($value);
+trait Hidrator {
+    public function hidrate(array $values) {
+        foreach ($values as $attr => $value) {
+            $methodSet = 'set' . ucfirst($attr);
+            if (method_exists($this, $methodSet)) {
+                $this->{$methodSet}($value);
             }
         }
     }
