690b34f3dc43c03298c12e5a04318fec1cf8f7ba
Guilherme Rodrigues <grodrigues@uolinc.com>
2019-09-13 14:14:11 -0300

    Alteração na forma de buildar o objeto

---
+++ src/Builder.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace Builder;
+
+trait Builder
+{
+
+    /**
+     * @param array $attributes
+     * @return object
+     * @throws \ReflectionException
+     */
+    public static function buildAssoc(array $attributes)
+    {
+        $reflector = new \ReflectionClass(self::class);
+
+        $parametersToBuild = self::retrieveParametersToBuild($reflector, $attributes);
+
+        return $reflector->newInstanceArgs($parametersToBuild);
+    }
+
+    /**
+     * @param \ReflectionClass $reflector
+     * @param array $attributes
+     * @return array
+     * @throws \Exception
+     */
+    private static function retrieveParametersToBuild(\ReflectionClass $reflector, array $attributes): array
+    {
+        $constructor = $reflector->getConstructor();
+
+        $parameters = $constructor->getParameters();
+
+        call_user_func('verifyRequiredParameters', $attributes, $parameters);
+
+        $parametersToBuild = [];
+
+        foreach($parameters as $parameter) {
+            $attributeType = self::getType($attributes[$parameter->getName()]);
+
+            if ($attributeType === 'object') {
+                $class = $parameter->getType()->getName();
+                if ($attributes[$parameter->getName()] instanceof $class) {
+                    throw new \Exception(
+                        sprintf('Invalid type from parameter %s', $parameter->getName())
+                    );
+                }
+            }
+
+            if ($attributeType!== $parameter->getType()->getName()) {
+                throw new \Exception(
+                    sprintf('Invalid type from parameter %s', $parameter->getName())
+                );
+            }
+
+            $parametersToBuild[$parameter->getPosition()] = $attributes[$parameter->getName()];
+        }
+
+        return $parametersToBuild;
+    }
+
+    /**
+     * @param mixed $var
+     * @return string
+     */
+    private static function getType($var): string
+    {
+        if (gettype($var) === 'integer') {
+            return 'int';
+        }
+
+        return gettype($var);
+    }
+
+    private function verifyRequiredParameters(array $attributes, array $parameters)
+    {
+        foreach ($parameters as $parameter) {
+            /**
+             * @var $parameter \ReflectionParameter
+             */
+            if (!$parameter->isOptional() && !isset($attributes[$parameter->getName()])) {
+                throw new \Exception(
+                    sprintf('The parameter %s is required', $parameter->getName())
+                );
+            }
+        }
+    }
+}
